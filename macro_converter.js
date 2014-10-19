38e47d357cb6383be9a8d6008e376b25dfeac56d
Ben Salinas <ben.salinas@gmail.com>
2014-10-19 12:24:16 -0400

    Initial Commit

---
+++ macro_converter.js
@@ -0,0 +1,1155 @@
+var origMacro = [
+	{
+		"args":
+		{
+			"characters": "<ul>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "</ul>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": ""
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "<li>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "<"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " class="
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "\"$0\""
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"characters": "large"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "characters",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": ">$"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "3.19</d"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "iv>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n<div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " class="
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "\"$0\""
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"characters": "small"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "characters",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": ">$"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "Regular</di"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "v>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "</li>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n<li>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "<div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " class="
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "\"$0\""
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"characters": "large"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "characters",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": ">$"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "3.39</div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": ">"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n<div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " class="
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "\"$0\""
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"characters": "small"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "characters",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": ">"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "Medium</div>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "<li"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "/li>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n<li>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "<div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " class="
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "\"$0\""
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"characters": "large"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "characters",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": ">$4"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "3.59</div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": ">"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n<div"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " class="
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "\"$0\""
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"characters": "small"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "characters",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": ">Premium"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "</div>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "</"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "/li>"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": false
+		},
+		"command": "move"
+	},
+	{
+		"args": null,
+		"command": "reindent"
+	},
+	{
+		"args":
+		{
+			"characters": "ul"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "{$0}"
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"file": "res://Packages/Default/Add Line in Braces.sublime-macro"
+		},
+		"command": "run_macro_file"
+	},
+	{
+		"args":
+		{
+			"characters": "list-style-"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "type:none;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": "\nli"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "{$0}"
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"file": "res://Packages/Default/Add Line in Braces.sublime-macro"
+		},
+		"command": "run_macro_file"
+	},
+	{
+		"args":
+		{
+			"characters": "d"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "isplay:inline-b"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "lock;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\nbackground-"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "color:"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " #f2f2f2"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": ";"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": "\n.large"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "{$0}"
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"file": "res://Packages/Default/Add Line in Braces.sublime-macro"
+		},
+		"command": "run_macro_file"
+	},
+	{
+		"args":
+		{
+			"characters": "font"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "-size:"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " 30px;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "padding:"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " 15px"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " 2px;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"by": "lines",
+			"forward": true
+		},
+		"command": "move"
+	},
+	{
+		"args":
+		{
+			"characters": "\n.small"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"contents": "{$0}"
+		},
+		"command": "insert_snippet"
+	},
+	{
+		"args":
+		{
+			"file": "res://Packages/Default/Add Line in Braces.sublime-macro"
+		},
+		"command": "run_macro_file"
+	},
+	{
+		"args":
+		{
+			"characters": "background-color"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": ":"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " #888;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\ncolo"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "r:"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " white;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\nfo"
+		},
+		"command": "insert"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args": null,
+		"command": "left_delete"
+	},
+	{
+		"args":
+		{
+			"characters": "text-align:ce"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "nter;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\n"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "text-transform:"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " uppercase;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\nfont"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "-size:"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "12px;"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "\npaddin"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": "g:2px"
+		},
+		"command": "insert"
+	},
+	{
+		"args":
+		{
+			"characters": " 0;"
+		},
+		"command": "insert"
+	}
+]
+
+var fs = require('fs')
+
+var commands = {
+	"insert": function(args){
+		var toRet = []
+		// var textObj = {text:args['characters']}
+		// toRet.push('type: '+JSON.stringify(textObj));
+		// return toRet;
+		if(args['characters'] == '\n'){
+			toRet.push({action:'run',arguments:{command:"newlineAndIndent"}})
+			// toRet.push({action:'run',arguments:{command:"indentAuto"}})
+			return toRet;
+		}
+		var parts = args['characters'].split('\n');
+		for(var j=0; j<parts.length; j++){
+			if(parts[j].length > 0){
+				var textObj = {text:parts[j]}
+				toRet.push({action:"type", arguments:textObj})
+			}
+			toRet.push({action:'type',arguments:{text:"\n"}});
+			// toRet.push({action:'run',arguments:{command:"newlineAndIndent"}})
+			toRet.push({action:'run',arguments:{command:"indentAuto"}})
+		}
+		toRet.pop();
+		toRet.pop();
+		return toRet;
+	},
+	"left_delete": function(args){
+		return [{action:'run',arguments:{command:"delCharBefore"}}]
+	},
+	"right_delete": function(args){
+		return [{action:'run',arguments:{command:"delCharAfter"}}]
+	},
+	"insert_snippet":function(args){
+		var toRet = []
+		var parts = args['contents'].split('$0');
+		if(parts.length > 0){
+			var j=0;
+			var charCount = 0;
+			for(j=0; j<parts.length; j++){
+				var textObj = {text:parts[j]}
+				toRet.push({action:'type',arguments:textObj})
+				charCount += parts[j].length;
+			}
+			//Now, we need to go back to where we were at the start.
+			charCount -= parts[0].length;
+			for(var k=0; k<charCount; k++){
+				toRet.push({action:'run',arguments:{command:"goCharLeft"}})	
+			}
+			
+		}
+		else{
+			console.error("Unknown Command: insert_snippet" + JSON.stringify(args));
+		}
+		return toRet;
+	},
+	"move": function(args){
+		if(args['by'] == 'characters'){
+			if(args['forward']){
+				return [{action:'run',arguments:{command:"goCharRight"}}];
+			}
+			else{
+				return [{action:'run',arguments:{command:"goCharLeft"}}]
+			}
+		}
+		if(args['by'] == 'lines'){
+			if(args['forward']){
+				return [{action:'run',arguments:{command:"goLineDown"}}];
+			}
+			else{
+				return [{action:'run',arguments:{command:"goLineUp"}}]
+			}
+		}
+		console.error("Unknown Command: move with args" + JSON.stringify(args));
+		return [];
+	},
+	"move_to": function(args){
+		if(args['to'] == 'bol'){
+			return [{action:'run',arguments:{command:"goLineStartSmart"}}]
+		}
+		else{
+			return [{action:'run',arguments:{command:"goLineEnd"}}];	
+		}
+	},
+	"run_macro_file": function(args){
+		var fileLoc = args['file'];
+		fileLoc = fileLoc.replace('res://', '/Users/ben/Library/Application\ Support/Sublime\ Text\ 2/');
+		var file = (JSON.parse(fs.readFileSync(fileLoc, "utf8")));
+		var toRet = parseMacro(file);
+		return toRet;
+	},
+	"reindent": function(args){
+		return [{action:'run',arguments:{command:"indentAuto"}}];
+	}
+
+}
+var parseMacro = function(steps){
+	console.log('Parse Macro With '+steps.length);
+	var toRet = []
+	var i=0;
+	for(i=0; i<steps.length; i++){
+		var step = steps[i];
+		var command = step['command'];
+		if(commands[command]){
+			var thisRet = commands[command](step['args']);
+			for(var j=0; j<thisRet.length; j++){
+				toRet.push(thisRet[j]);
+			}
+		}
+		else{
+			console.error('No command for '+command);
+		}
+	}
+	return toRet;
+}
+var collapseSteps = function(steps){
+	var toRet = []
+	toRet.push(steps[0])
+	for(var i=1; i<steps.length; i++){
+		var thisStep = steps[i];
+		var lastStep = toRet[toRet.length -1];
+		if(lastStep['action'] == thisStep['action']){
+			if(thisStep['action'] == 'run'){
+				if(thisStep['arguments']['command'] == lastStep['arguments']['command']){
+					if(lastStep['arguments']['times']){
+						lastStep['arguments']['times'] = ""+ (parseInt(lastStep['arguments']['times'])+1);
+						toRet[toRet.length -1] = lastStep;
+						continue;
+					}
+					else{
+						lastStep['arguments']['times'] = "2";
+						toRet[toRet.length -1] = lastStep;	
+						continue;
+					}
+				}
+			}
+			if(thisStep['action'] == 'type'){
+				lastStep['arguments']['text'] = lastStep['arguments']['text'] + thisStep['arguments']['text'];
+				toRet[toRet.length -1] = lastStep;
+				continue;
+			}
+		}
+		toRet.push(thisStep);
+	}
+	return toRet;
+}
+var ret = parseMacro(origMacro);
+var collapsed = collapseSteps(ret);
+for(i=0; i<collapsed.length; i++){
+	console.log(collapsed[i]['action']+': '+JSON.stringify(collapsed[i]['arguments']))
+}
